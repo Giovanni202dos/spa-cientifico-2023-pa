11d309bfee269876b6575f68ec4319f822589dee
andres <palaciosgiovanniandres1999@gmail.com>
2026-04-25 00:09:27 -0300

    agregando una api como proxy para vercel ya que saltaba error de cors2

--- api/rm.js
+++ api/rm.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+module.exports = async (req, res) => {
   const query = req.url.replace("/api/rm", "")
   const url = `https://rickandmortyapi.com/api${query}`
 
