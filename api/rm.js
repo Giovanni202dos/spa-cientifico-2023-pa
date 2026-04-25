2b351012247cf1fb5cd437a1308fdb73aba1c88c
andres <palaciosgiovanniandres1999@gmail.com>
2026-04-25 00:26:34 -0300

    agregando una api como proxy para vercel ya que saltaba error de cor7

--- api/rm.js
+++ api/rm.js
@@ -1,13 +1,21 @@
 module.exports = async (req, res) => {
-  const query = req.url.replace("/api/rm", "")
-  const url = `https://rickandmortyapi.com/api${query}`
-
   try {
-    const response = await fetch(url)
+    const url = new URL(req.url, `http://${req.headers.host}`)
+
+    const path = url.pathname.replace("/api/rm", "")
+    const search = url.search
+
+    const finalUrl = `https://rickandmortyapi.com/api${path}${search}`
+
+    console.log("Fetching:", finalUrl)
+
+    const response = await fetch(finalUrl)
     const data = await response.json()
 
     res.status(200).json(data)
+
   } catch (error) {
+    console.error("ERROR BACKEND:", error)
     res.status(500).json({ error: "Error fetching data" })
   }
 }
